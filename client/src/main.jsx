2326bbe5ea04ce3a2a7e604ea5f745c358d23b51
ahmad kmail <ahmad.kmail19996@gmail.com>
2024-09-16 16:56:13 +0300

    adding redux persist to connect the localStorge

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,15 @@
-import { StrictMode } from 'react'
+// eslint-disable-next-line no-unused-vars
+import {PersistGate} from 'redux-persist/integration/react'
+// Used in JSX below
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { store } from './redux/store'
+import { persistor, store } from './redux/store'
 import { Provider } from 'react-redux'
 createRoot(document.getElementById('root')).render(
   <Provider store ={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
 )
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,22 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import user from './user/userSlice'
+import { persistReducer, persistStore} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const rootReducer = combineReducers({user});
+const presistedConfige = {
+  key: 'root',
+  storage,
+  version:1
+}
+const presistedreducer = persistReducer(presistedConfige,rootReducer )
 export const store = configureStore({
-  reducer: {user},
+  reducer: presistedreducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 })
+
+export const persistor = persistStore(store)
 
